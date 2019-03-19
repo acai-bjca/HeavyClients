65704c0e805930ccd6d1925ad376588c1b12821e
acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
2019-03-18 23:53:59 -0500

    Amalia y Valentina - Parte uno terminada y se pueden ver las sillas

--- REST_API-SpringBoot-Cinema/src/main/resources/static/js/app.js
+++ REST_API-SpringBoot-Cinema/src/main/resources/static/js/app.js
@@ -1,28 +1,83 @@
-//@author dsaavedra
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+var funciones;
+var sillas;
+
+function llenarTabla() {    
+    for (i = 0; i < funciones.length; i++) {
+        var numFun = i+1;
+        var pelicula = funciones[i].movie.name;
+        var sillas = sillasDisponibles(funciones[i].seats);
+        var fecha = funciones[i].date;
+        var fila = "<tr><td >" + numFun + "</td><td><input type='checkbox' name='record'></td><td>" + pelicula + "</td><td>" + sillas + "</td><td>" + fecha + "</td></tr>";
+        $("#tb1").append(fila);        
+    }
 }
-var nombreCine;
 
+function mostrarSala() {     
+    $("#tb1").find('input[name="record"]').each(function(){
+        if($(this).is(":checked")){   
+            var numFun = ("td:first-child").value();
+            //var numFun =  $(this).parents("tr").values();
+            var obj = JSON.parse(numFun);
+            alert(numFun);
+        }
+    });
+    sillas=[[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, false, true, true, true, true, true, true, true, false], [true, true, false, true, true, true, false, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, false, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]];
+    
+    var imgOcupada = "<img src=\"../imagenes/sillaGris.png\" style=\"width: 60px; height: 60px\">"; 
+    var imgDisponible = "<img src=\"../imagenes/sillaAzul.png\" style=\"width: 60px; height: 60px\">";     
+    var divMain = document.getElementById("sala");
+    var tablaSala = document.getElementById("tablaSillas");
+    var tblBody = document.createElement("tbody");
+    
+    tablaSala.appendChild(tblBody);
+    
+    for (i = 0; i < sillas.length; i++) {
+        var fila = document.createElement('tr');
+        var fila = "<tr>";
+        for (j = 0; j < sillas[i].length; j++) {
+            if(sillas[i][j] == true){
+                fila += "<td>"+imgDisponible+"</td>";
+            }else{
+                fila += "<td>"+imgOcupada+"</td>";
+            }
+        }
+        fila+="</tr>";
+        $("#tbody").append(fila);
+    }
+    //divMain.appendChild(tablaSala);
+}
 
-app = (function () {    
+function sillasDisponibles(sillas) {
+    var cont = 0;
+    for (var i = 1; i < sillas.length; i++) {
+        for (var j = 1; j < sillas[i].length; j++) {
+            if (sillas[i][j] == true) {
+                cont++;
+            }
+        }
+    }
+    return cont;
+}
+
+app = (function () {
     var nameCinema;
     var functionscinema;
 
     return {
-    setCinemaName: function(){
-        nameCinema = document.getElementById("txt_cine").value;        
-    },        
-    
-    obtenerFunciones: function(cine){
-        functionscinema = cine.functions;
-        //document.getElementById("funcionesTxt").innerHTML = functionscinema[0].movie.name;
-        document.getElementById("funcionesTxt").innerHTML = functionscinema[0].getPrototypeOf().toString();
-    },
-    
-    buscarFunciones: function () {
-        app.setCinemaName();
-        apimock.getCinemaByName(nameCinema, app.obtenerFunciones);
-    }       
+        setCinemaName: function () {
+            nameCinema = document.getElementById("txt_cine").value;
+        },
+
+        obtenerFunciones: function (cine) {
+            functionscinema = cine.functions;
+            funciones = functionscinema;
+            llenarTabla();
+            document.getElementById("funcionesTxt").innerHTML = functionscinema[0].movie.name;
+        },
+
+        buscarFunciones: function () {
+            app.setCinemaName();
+            apimock.getCinemaByName(nameCinema, app.obtenerFunciones);
+        }
     };
 })();
--- REST_API-SpringBoot-Cinema/src/main/resources/static/js/app_1.js
+++ REST_API-SpringBoot-Cinema/src/main/resources/static/js/app_1.js
@@ -1,24 +1,17 @@
-function saludar(nombre) {
-  alert('Hola ' + nombre);
-}
-function obtenerFunciones(cine){
-    document.getElementById("funcionesTxt").innerHTML = "BuscandoFunciones2";
-    objCine = JSON.parse(cine);       
-    functionscinema = objCine.functions; 
-    document.getElementById("funcionesTxt").innerHTML = "functionscinema";
+$(document).ready(function () {
+    $(".add-row").click(function () {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
+        $("#tb1").append(markup);
+    });
 
- } 
-
-function getCinemaByName(cinema, callback) {
-    var mockdata = [];
-    mockdata["Cine80"] = [{ "name": "Cine80", "functions": [{ "movie": { "name": "Titanic Movie", "genre": "Action" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2018-12-18 15:30" }, { "movie": { "name": "The Purge", "genre": "Horror" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2018-12-18 15:30" }] }];
-    mockdata["Cine112"] = [{ "name": "Cine112", "functions": [{ "movie": { "name": "mision imposible", "genre": "Fiction" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2018-12-18 15:31" }] }];
-    mockdata["Cinemania"] = [{ "name": "Cinemania", "functions": [{ "movie": { "name": "la favorita", "genre": "Drama" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2019-03-14 15:31" }] }];
-    mockdata["CineColombia"] = [{ "name": "CineColombia", "functions": [{ "movie": { "name": "capitana marvel", "genre": "Aventure" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2019-03-14 01:30" }] }];
-    
-    document.getElementById("funcionesTxt").innerHTML = "APIMOCK";
-    cine = mockdata[name];            
-    callback(cine);
-}
-
-procesarEntradaUsuario(saludar);
+// Find and remove selected table rows
+    $(".delete-row").click(function () {
+        $("table tbody").find('input[name="record"]').each(function () {
+            if ($(this).is(":checked")) {
+                $(this).parents("tr").remove();
+            }
+        });
+    });
+});   
